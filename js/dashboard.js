85c38d19b297e2a70db38e5eaa811241a0212d6c
janu1213 <g.b.janavi@gmail.com>
2020-09-19 10:59:18 +0530

    dashboard js

--- js/dashboard.js
+++ js/dashboard.js
@@ -16,3 +16,33 @@ function upload_fac(){
 function search_fac() {
   window.location.href = "search.html";
 }
+
+function complaint() {
+  var txt;
+  var person = prompt("Please enter your complaint:","Here");
+  if (person == null || person == ""||person=="Here") {
+    alertnot()
+  } else {
+    alertok()
+  }
+  
+}
+function alertok() {
+  var con;
+  if (confirm("Complaint submitted")) {
+    con = 1;
+  } 
+  else{
+   con = 0;
+  }
+  
+}
+function alertnot() {
+  var conn;
+  if (confirm("Complaint cancelled")) {
+    conn = 1;
+  } else {
+    conn = 0;
+  }
+  
+}
